dbb1a05c6ac439522ba650b7a9e6c6bcba954768
Tu nombre <iv@gmail.com>
2024-12-21 10:14:15 +0100

    He borrado archivos de javafx para su posterior creacion y he modificado el pom

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,8 @@
-module es.etg.tierno.prog.simulacro {
+/*module es.etg.tierno.prog.simulacro {
     requires javafx.controls;
     requires javafx.fxml;
 
     opens es.etg.tierno.prog.simulacro to javafx.fxml;
     exports es.etg.tierno.prog.simulacro;
 }
+*/
